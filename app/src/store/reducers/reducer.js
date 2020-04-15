0de2d15fb7d2b480f10a9e6487f1f37533a67108
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-15 18:27:12 -0400

    Actions and a reducer are set up now.

--- app/src/store/reducers/reducer.js
+++ app/src/store/reducers/reducer.js
@@ -1,7 +1,35 @@
-export const initialState = {};
+import {
+  FETCH_DATA_START,
+  FETCH_DATA_SUCCESS,
+  FETCH_DATA_FAILURE,
+} from "../actions/actions";
+
+export const initialState = {
+  data: {},
+  isFetching: false,
+  error: "",
+};
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_DATA_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case FETCH_DATA_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        data: action.payload,
+        error: "",
+      };
+    case FETCH_DATA_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
